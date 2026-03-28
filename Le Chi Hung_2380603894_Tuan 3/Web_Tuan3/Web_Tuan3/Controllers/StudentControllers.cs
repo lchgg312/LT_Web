830e92a425adadb26f892275a180f12f3b96dbb3
Hung Le <lchgg312@gmail.com>
2026-03-28 20:02:22 +0700

    Add files via upload

---
+++ Le Chi Hung_2380603894_Tuan 3/Web_Tuan3/Web_Tuan3/Controllers/StudentControllers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Tuan3.Models;
+
+namespace Web_Tuan3.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<StudentModel> listStudents = new List<StudentModel>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowKQ(StudentModel student)
+        {
+            listStudents.Add(student);
+
+            int countSameMajor = listStudents.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+            ViewBag.Count = countSameMajor;
+
+            return View(student);
+        }
+
+        public IActionResult List()
+        {
+            return View(listStudents);
+        }
+    }
+}
+
---
+++ Le Chi Hung_2380603894_Tuan 3/Web_Tuan3/Web_Tuan3/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Tuan3.Models
+{
+    public class StudentModel
+    {
+        public string MSSV { get; set; }
+        public string HoTen { get; set; }
+        public double DiemTB { get; set; }
+        public string ChuyenNganh { get; set; }
+    }
+}
